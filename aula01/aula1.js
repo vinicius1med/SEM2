46e4dfbe007c15bde4f753928aa24f04d5dc6649
Vini <majorvinimedina1@gmail.com>
2023-09-13 16:04:31 -0300

    att02

---
+++ aula01/aula1.js
@@ -0,0 +1,22 @@
+var a = 10 // inteiro
+var b = '20'
+var c = false
+
+a = 'beto' // troca de tipagem -> objeto string
+
+let x = 20
+
+const y = 'Victor' // Isso eh uma constante 
+
+// -------------------------------------------------------
+var d // eh do tipo undefined - não foi inicializada 
+var e = '' // none | null -> variável não tem valor
+
+var arr = new Array()
+
+var arr2 = new Array(1, 2, 3, 4)
+
+var arr3 = [1, 2, 3, 4]
+
+console.log (arr3);
+
